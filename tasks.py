fca46e77686a16f6e2a42ee3a2e57363af9cc1bf
omussell <oliver.mussell@hotmail.co.uk>
2019-09-14 18:25:39 +0100

    Fix typos and PATH

--- tasks.py
+++ tasks.py
@@ -1,23 +1,24 @@
-from invoke import tasks
+from invoke import task
 from pathlib import Path
 
 py_ver = '3.6'
 venv_path = Path.home().joinpath('venv')
 env_vars = { 
-        'PATH': f"{venv_path}",
+        'PATH': f"{venv_path}:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/home/oem/.local/bin",
+        # make pipenv create a .venv folder in the project dir
         'PIPENV_VENV_IN_PROJECT': 'YES'
         }
 
 @task
 def venv_create(c):
     if not venv_path.is_file():
-        c.run(f"python{py_ver} -m venv {venv_path")
+        c.run(f"python{py_ver} -m venv {venv_path}")
 
 @task
 def pip_clean(c):
     c.run(f"pipenv clean", env=env_vars)
 
-@task:
+@task
 def pip_install(c):
     c.run(f"pipenv install --deploy --sequential", env=env_vars)
 
